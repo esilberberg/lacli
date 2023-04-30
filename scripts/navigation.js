aadb54870ffa6767be83b9a066b00a5abe7aaf1f
esilberberg <ericsil218@gmail.com>
2023-04-30 14:13:22 -0400

    return to top btn

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,4 @@
-// ----- NAVIGATION -----
+//Mobile responsive hamburger nav menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -20,3 +20,18 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     })
 );
 
+// Return to top button on library.html
+const topButton = document.getElementById('topButton');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 100) {
+    topButton.style.display = 'block';
+  } else {
+    topButton.style.display = 'none';
+  }
+});
+
+topButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+  
