a7012c3e3dd8dc9a5b5afdf35f674224cf3c95e2
esilberberg <ericsil218@gmail.com>
2023-06-26 20:08:10 -0400

    stevieP idea load data on pg load

--- scripts/app.js
+++ scripts/app.js
@@ -22,16 +22,76 @@ function removeDiacritics(str) {
 }
 
 // Fetch data from Google Sheets API
+let initialData = [];
+
 async function fetchData(url) {
+  try {
     const response = await fetch(url);
     const data = await response.json();
-    return data;
+    initialData = data;
+    filterData()  
+  } catch (error) {
+    alert('Error fetching data');
+  }
 }
 
+fetchData(apiEndpoint);
+
+function filterData(searchQuery) {
+
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const urlSearchQuery= urlSearchParams.get('q');
+
+    if (searchQuery) {
+        const searchTerms = searchQuery.toLowerCase().split(/\s+/).map(term => removeDiacritics(term));
+
+        const filteredData = initialData.filter(eventData => {
+        return searchTerms.every(term => {
+            return Object.values(eventData).some(value => {
+            return value && removeDiacritics(value.toString().toLowerCase()).includes(term);
+            });
+        });
+        });
+
+        displayData(filteredData, searchQuery);
+    } else if (urlSearchQuery) {
+        const searchTerms = urlSearchQuery.toLowerCase().split(/\s+/).map(term => removeDiacritics(term));
+        const filteredData = initialData.filter(eventData => {
+        return searchTerms.every(term => {
+            return Object.values(eventData).some(value => {
+            return value && removeDiacritics(value.toString().toLowerCase()).includes(term);
+            });
+        });
+        });
+
+        displayData(filteredData, urlSearchQuery);
+
+    } else {
+        displayData(initialData, '');
+    }
+}
+
+function runSearch() {
+    const searchQuery = search.value.trim();
+    filterData(searchQuery);
+
+    // Update URL with search query
+    const newURL = new URL(window.location.href);
+    newURL.searchParams.set('q', searchQuery);
+    window.history.pushState(null, '', newURL);
+};
+
+// Event listeners for search bar
+librarySearchBtn.addEventListener('click', runSearch);
+search.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        runSearch();
+    }
+});
 
 function displayData(data, searchQuery) {
     storeData(data);
-    // Generate HTML for filtered data and display on the page
+    loader.style.display = 'none';
     let counter = 1;
     let dataDisplay = data.map((object)=>{
         let creatorsField = object.Creators ? `
@@ -90,6 +150,7 @@ function displayData(data, searchQuery) {
         `
     }).join('');
 
+
     // Print data to HTML
     display.innerHTML = dataDisplay;
 
@@ -114,110 +175,34 @@ function displayData(data, searchQuery) {
         }
     });
     }
-
-    // Show display
-    display.style.display = "block";
 }
 
 
-// Filter data based on search parameters
-async function filterData(url, searchQuery) {
-    // Show loading animation
-    loader.style.display = 'block';
-    display.style.display = 'none';
-  
-    // Fetch data from API
-    const payload = await fetchData(url);
-  
-    if (!searchQuery) {
-      // If no search query provided, return all data
-      loader.style.display = 'none';
-      displayData(payload, '');
-      return;
-    }
-  
-    const searchTerms = searchQuery.toLowerCase().split(/\s+/).map(term => removeDiacritics(term));
-    const filteredData = payload.filter(eventData => {
-      if (!searchQuery) {
-        return true;
-      } else {
-        return searchTerms.every(term => {
-          return Object.values(eventData).some(value => {
-            return value && removeDiacritics(value.toString().toLowerCase()).includes(term);
-          });
-        });
-      }
-    });
-  
-    // Hide loader
-    loader.style.display = 'none';
-  
-    // Display filtered data
-    displayData(filteredData, searchQuery);
-  }
-  
+// Add an event listener to the random button
+randomBtn.addEventListener('click', getRandomResource);
 
-async function getRandomResource() {
-    // Show loading animation
-    loader.style.display = 'block';
-    display.style.display = 'none';
-
-    const data = await fetchData(apiEndpoint);
-    const randomIndex = Math.floor(Math.random() * data.length);
-    const randomResource = data[randomIndex]
+// Get random resource functionality
+function getRandomResource() {
+    const randomIndex = Math.floor(Math.random() * initialData.length);
+    const randomResource = initialData[randomIndex]
     displayData([randomResource], randomResource.Resource_Title)
 
     const newURL = new URL(window.location.href);
     newURL.searchParams.set('q', randomResource.Resource_Title);
     window.history.pushState(null, '', newURL);
-
-    // Hide loader
-    loader.style.display = 'none';
   }
 
-// Add an event listener to the button element
-randomBtn.addEventListener('click', async () => {
-    // Call the getRandomItem function and log the result
-    const randomItem = await getRandomResource();
-  });
 
+// Add an event listener to the export btn
+exportBtn.addEventListener('click', exportJSON);
 
-// Filter data based on search query from URL or search bar input
-function runSearch() {
-    const searchQuery = search.value.trim();
-    filterData(apiEndpoint, searchQuery);
-
-    // Update URL with search query
-    const newURL = new URL(window.location.href);
-    newURL.searchParams.set('q', searchQuery);
-    window.history.pushState(null, '', newURL);
-}
-
-
-// Event listener for librarySearchBtn click
-librarySearchBtn.addEventListener('click', () => {
-    runSearch();
-});
-
-// Event listener for search input keypress
-search.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-        runSearch();
-    }
-});
-
-
-// Run initial search based on URL search terms
-filterData(apiEndpoint, searchParams.get('q'));
-
-// Export to a JSON file functionality
-async function storeData(data) {
+// Store data for JSON export
+function storeData(data) {
     const dataAsString = JSON.stringify(data);
     sessionStorage.setItem('data', dataAsString);
 }
 
-exportBtn.addEventListener('click', exportJSON);
-
+// Export current results to JSON file
 function exportJSON() {
     const data = JSON.parse(sessionStorage.getItem('data'));
     const jsonData = JSON.stringify(data, null, 2);
@@ -231,5 +216,3 @@ function exportJSON() {
     link.click();
     document.body.removeChild(link);
   }
-  
-  
