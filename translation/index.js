769086655582629bb3c3c28b7924c3810d8cdb09
esilberberg <ericsil218@gmail.com>
2023-07-26 00:29:56 -0400

    started translation script

---
+++ translation/index.js
@@ -0,0 +1,112 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const languageNavbar = document.getElementById('language-navbar');
+    const hero = document.getElementById('hero-text');
+    const search = document.getElementById('index-search');
+    const students = document.getElementById('students');
+    const studentsDescription = document.getElementById('students-description');
+    const faculty = document.getElementById('faculty');
+    const facultyDescription = document.getElementById('faculty-description');
+    const librarians = document.getElementById('librarians');
+    const librariansDescription = document.getElementById('librarians-description');
+    const digitalScholars = document.getElementById('digital-scholars');
+    const digitalScholarsDescription = document.getElementById('digital-scholars-description');
+    const developedBy = document.getElementById('developed-by');
+    const collaboratingInstitutions = document.getElementById('collaborating-institutions');
+
+    const storedLanguage = localStorage.getItem('lacliLanguagePreference');
+
+    if (storedLanguage) {
+      updateLanguageSelection(storedLanguage);
+      updateContentLanguage(storedLanguage);
+    }
+  
+    function updateLanguageSelection(language) {
+      const languageLinks = languageNavbar.getElementsByTagName('a');
+      for (let i = 0; i < languageLinks.length; i++) {
+        if (languageLinks[i].getAttribute('data-lang') === language) {
+          languageLinks[i].classList.add('lang-link-active');
+        } else {
+          languageLinks[i].classList.remove('lang-link-active');
+        }
+      }
+    }
+  
+
+    function updateContentLanguage(language) {
+      const translations = {
+        en: {
+          hero: 'Explore a repository of free online resources for Latin American, Caribbean, Latinx, and Iberian studies.',
+          placeholder: 'Search...',
+          students: 'Students',
+          studentsDescription: 'Explore collections by subject, country, language, time period and more to support original research projects.',
+          faculty: 'Faculty',
+          facultyDescription: 'Enhance courses with primary sources, reference materials, scholarly articles, and more!',
+          librarians: 'Librarians',
+          librariansDescription: 'Discover resources for research consultations and develop subject or country-specific research guides.',
+          digitalScholars: 'Digital Scholars',
+          digitalScholarsDescription: 'Fuel your digital scholarship projects with open data and primary source collections.',
+          developedBy: 'Developed by',
+          collaboratingInstitutions: 'Collaborating Institutions',
+        },
+        es: {
+          hero: 'Explore un repositorio de recursos gratuitos en línea para estudios sobre América Latina, el Caribe, los Latinos de los Estados Unidos, y la Península Ibérica.',
+          placeholder: 'Buscar...',
+          students: 'Estudiantes',
+          studentsDescription: 'Explore colecciones organizadas por tema, país, idioma, período de tiempo y más, para apoyar proyectos de investigación originales.',
+          faculty: 'Profesorado',
+          facultyDescription: 'Mejore sus cursos con fuentes primarias, materiales de referencia, artículos académicos ¡y mucho más!',
+          librarians: 'Bibliotecarios',
+          librariansDescription: 'Descubra recursos para consultas de investigación y elabore guías de investigación específicas por temas o países.',
+          digitalScholars: 'Académicos digitales',
+          digitalScholarsDescription: 'Impulse sus proyectos académicos digitales con datos abiertos y colecciones de fuentes primarias.',
+          developedBy: 'Desarrollado por',
+          collaboratingInstitutions: 'Instituciones colaboradoras',
+        },
+        pt: {
+          hero: 'Pesquise em um repositório com recursos online e abertos sobre América Latina, Caribe, Estudos Ibéricos e Latinos.',
+          placeholder: 'Pesquisar...',
+          students: 'Estudantes',
+          studentsDescription: 'Explore coleções por assuntos, país, idioma, recorte temporal e outras fontes para auxiliar nos projetos de pesquisa.',
+          faculty: 'Professores',
+          facultyDescription: 'Aprimore o ensino utilizando fontes primárias, materiais de referência, artigos acadêmicos e mais!',
+          librarians: 'Bibliotecários',
+          librariansDescription: 'Descubra recursos para seus usuários e desenvolva guias de pesquisa por assunto ou país.',
+          digitalScholars: 'Pesquisadores',
+          digitalScholarsDescription: 'Incremente seus projetos de pesquisa e em humanidades digitais com dados abertos e coleções de fontes primárias',
+          developedBy: 'Desenvolvido por',
+          collaboratingInstitutions: 'Instituições colaboradoras',
+        },
+      };
+  
+      hero.textContent = translations[language].hero;
+      search.placeholder = translations[language].placeholder;
+      students.textContent = translations[language].students;
+      studentsDescription.textContent = translations[language].studentsDescription;
+      faculty.textContent = translations[language].faculty;
+      facultyDescription.textContent = translations[language].facultyDescription;
+      librarians.textContent = translations[language].librarians;
+      librariansDescription.textContent = translations[language].librariansDescription;
+      digitalScholars.textContent = translations[language].digitalScholars;
+      digitalScholarsDescription.textContent = translations[language].digitalScholarsDescription;
+      developedBy.textContent = translations[language].developedBy;
+      collaboratingInstitutions.textContent = translations[language].collaboratingInstitutions;
+    }
+  
+    languageNavbar.addEventListener('click', function (event) {
+      event.preventDefault();
+      const selectedLanguage = event.target.getAttribute('data-lang');
+      localStorage.setItem('lacliLanguagePreference', selectedLanguage);
+      updateLanguageSelection(selectedLanguage);
+      updateContentLanguage(selectedLanguage);
+    });
+  
+    const footerLanguage = document.getElementById('footer-language');
+    footerLanguage.addEventListener('click', function (event) {
+      event.preventDefault();
+      const selectedLanguage = event.target.getAttribute('data-lang');
+      localStorage.setItem('lacliLanguagePreference', selectedLanguage);
+      updateLanguageSelection(selectedLanguage);
+      updateContentLanguage(selectedLanguage);
+    });
+});
+  
---
+++ translation/navbar.js
@@ -0,0 +1,127 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const languageNavbar = document.getElementById('language-navbar');
+  const navHome = document.getElementById('nav-home');
+  const navLibrary = document.getElementById('nav-library');
+  const navAbout = document.getElementById('nav-about');
+  const navContact = document.getElementById('nav-contact');
+  const navHelp = document.getElementById('nav-help');
+  const navContribute = document.getElementById('nav-contribute');
+
+  const footerHome = document.getElementById('footer-home');
+  const footerLibrary = document.getElementById('footer-library');
+  const footerAbout = document.getElementById('footer-about');
+  const footerContact = document.getElementById('footer-contact');
+  const footerHelp = document.getElementById('footer-help');
+  const footerContribute = document.getElementById('footer-contribute');
+  const footerOpenData = document.getElementById('footer-open-data');
+  const footerCCLicense1 = document.getElementById('cc-license-1');
+  const footerCCLicense2 = document.getElementById('cc-license-2');
+
+  const storedLanguage = localStorage.getItem('lacliLanguagePreference');
+
+  if (storedLanguage) {
+    updateLanguageSelection(storedLanguage);
+    updateContentLanguage(storedLanguage);
+  }
+
+  function updateLanguageSelection(language) {
+    const languageLinks = languageNavbar.getElementsByTagName('a');
+    const footerLanguage = document.getElementById('footer-language');
+    const footerLanguageLinks = footerLanguage.getElementsByTagName('a');
+
+    // Remove active class from navbar language links
+    for (let i = 0; i < languageLinks.length; i++) {
+      if (languageLinks[i].getAttribute('data-lang') === language) {
+        languageLinks[i].classList.add('lang-link-active');
+      } else {
+        languageLinks[i].classList.remove('lang-link-active');
+      }
+    }
+
+    // Add active class to footer language links
+    for (let i = 0; i < footerLanguageLinks.length; i++) {
+      if (footerLanguageLinks[i].getAttribute('data-lang') === language) {
+        footerLanguageLinks[i].classList.add('lang-link-active');
+      } else {
+        footerLanguageLinks[i].classList.remove('lang-link-active');
+      }
+    }
+  }
+  
+    function updateContentLanguage(language) {
+      const translations = {
+        en: {
+          home: 'Home',
+          library: 'Library',
+          about: 'About',
+          contact: 'Contact',
+          help: 'Help',
+          contribute: 'Contribute',
+          openData: 'Open Data',
+          ccLicense1: 'This work is licensed under a',
+          ccLicense2: 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License, 2023.',
+          ccLicenseURL: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en',
+        },
+        es: {
+          home: 'Inicio',
+          library: 'Biblioteca',
+          about: 'Acerca de',
+          contact: 'Contacto',
+          help: 'Ayuda',
+          contribute: 'Contribuye',
+          openData: 'Datos abiertos',
+          ccLicense1: 'Esta obra está bajo una',
+          ccLicense2: 'Licencia Creative Commons Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional, 2023.',
+          ccLicenseURL: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es',
+        },
+        pt: {
+          home: 'Principal',
+          library: 'Biblioteca',
+          about: 'Sobre',
+          contact: 'Contacto',
+          help: 'Ajuda',
+          contribute: 'Contribua',
+          openData: 'Dados abertos',
+          ccLicense1: 'Este trabalho está sob um',
+          ccLicense2: 'Licencia Creative Commons Atribuição-NãoComercial-CompartilhaIgual 4.0 Internacional, 2023.',
+          ccLicenseURL: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.pt_BR',
+        },
+      };
+
+      navHome.textContent = translations[language].home;
+      navLibrary.textContent = translations[language].library;
+      navAbout.textContent = translations[language].about;
+      navContact.textContent = translations[language].contact;
+      navHelp.textContent = translations[language].help;
+      navContribute.textContent = translations[language].contribute;
+
+      if (footerHome) footerHome.textContent = translations[language].home;
+      if (footerLibrary) footerLibrary.textContent = translations[language].library;
+      if (footerAbout) footerAbout.textContent = translations[language].about;
+      if (footerContact) footerContact.textContent = translations[language].contact;
+      if (footerHelp) footerHelp.textContent = translations[language].help;
+      if (footerContribute) footerContribute.textContent = translations[language].contribute;
+      if (footerOpenData) footerOpenData.textContent = translations[language].openData;
+      if (footerCCLicense1) footerCCLicense1.textContent = translations[language].ccLicense1;
+      if (footerCCLicense2) footerCCLicense2.textContent = translations[language].ccLicense2;
+      if (footerCCLicense2) footerCCLicense2.setAttribute('href', translations[language].ccLicenseURL);
+    }
+  
+    languageNavbar.addEventListener('click', function (event) {
+      event.preventDefault();
+      const selectedLanguage = event.target.getAttribute('data-lang');
+      localStorage.setItem('lacliLanguagePreference', selectedLanguage);
+      updateLanguageSelection(selectedLanguage);
+      updateContentLanguage(selectedLanguage);
+    });
+  
+    // Add event listener to footer language list
+    const footerLanguage = document.getElementById('footer-language');
+    footerLanguage.addEventListener('click', function (event) {
+      event.preventDefault();
+      const selectedLanguage = event.target.getAttribute('data-lang');
+      localStorage.setItem('lacliLanguagePreference', selectedLanguage);
+      updateLanguageSelection(selectedLanguage);
+      updateContentLanguage(selectedLanguage);
+    });
+});
